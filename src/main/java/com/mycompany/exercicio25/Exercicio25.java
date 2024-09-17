c2295ffbc37091cbf43aaa477701f4a2a0499bbe
ADM <ADM@10.119.164.113>
2024-09-17 20:57:32 -0300

    init: início do projeto

---
+++ src/main/java/com/mycompany/exercicio25/Exercicio25.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+
+public class Exercicio25 {
+
+    public static void main(String[] args) {
+        int qtde = 20;
+        int vetor[] = new int[qtde];
+        for (int i = 0; i < qtde; i++) {
+            vetor[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite o número :" + i));
+        }
+        int soma = 0;
+        for (int i = 0; i < qtde; i++) {
+            soma = soma + vetor[i];
+        }
+        double media = soma / qtde;
+        JOptionPane.showMessageDialog(null, "A soma é: " + soma + "\nA média é " + media);
+    }
+}
